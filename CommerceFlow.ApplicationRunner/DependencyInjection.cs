d652d6a2f11d0309f21b49fbdec93a39ad7885bc
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2026-06-16 17:15:11 -0300

    Refactor message bus publisher/subscriber configuration
    Separated publisher and subscriber registrations for ShipmentRequested, ShipmentDispatched, and ShipmentDelivered events. Added comments to clarify Orders and Shipments sections and included missing subscriptions for CommerceFlow.Orders events.

--- CommerceFlow.ApplicationRunner/DependencyInjection.cs
+++ CommerceFlow.ApplicationRunner/DependencyInjection.cs
@@ -14,23 +14,31 @@ public static class DependencyInjection
         builder.Services.AddTransient<ICarrierSelector, FakeCarrierSelector>();
         builder.ConfigureMessageBus(opts =>
         {
+            // Orders
             opts.Subscribe<CreateOrder>();
             opts.Subscribe<OrderCreated>();
             opts.Subscribe<OrderInventoryReserved>();
             opts.ConfigurePublisher<OrderWaitingForPayment>();
             opts.Subscribe<ApprovePayment>();
             opts.Subscribe<PaymentApproved>();
-            opts.ConfigurePublisher<CommerceFlow.Orders.ShipmentRequested>().Subscribe<CommerceFlow.Shipments.ShipmentRequested>();
+            opts.ConfigurePublisher<CommerceFlow.Orders.ShipmentRequested>();
+
+            // Shipments
+            opts.Subscribe<ShipmentRequested>();
             opts.Subscribe<ShipmentCreated>();
             opts.Subscribe<CarrierAssigned>();
             opts.Subscribe<CompletePacking>();
             opts.Subscribe<PackingCompleted>();
-            opts.ConfigurePublisher<CommerceFlow.Shipments.ShipmentDispatched>().Subscribe<CommerceFlow.Orders.ShipmentDispatched>();
+            opts.ConfigurePublisher<ShipmentDispatched>();
             opts.Subscribe<RegisterTrackingEvent>();
-            opts.ConfigurePublisher<CommerceFlow.Shipments.ShipmentDelivered>().Subscribe<CommerceFlow.Orders.ShipmentDelivered>();
             opts.Subscribe<DeliverShipment>();
+            opts.ConfigurePublisher<ShipmentDelivered>();
 
-            opts.ConfigurePublisher<OrderCancelled>();
+            // Orders - continued
+            opts.Subscribe<CommerceFlow.Orders.ShipmentDispatched>();            
+            opts.Subscribe<CommerceFlow.Orders.ShipmentDelivered>();            
+
+            opts.Subscribe<OrderCancelled>();
 
             opts.Discovery.IncludeAssembly(typeof(CreateOrderHandler).Assembly);
         });
