3ab9dee00155fb525251e4f357ddcffed17d0acd
Lucas Fogliarini <lucasfogliarini@gmail.com>
2026-07-24 09:50:39 -0300

    add context diagram

--- CommerceFlow.AppHost/CommerceFlowSystem/CommerceFlowSystemBuilder.cs
+++ CommerceFlow.AppHost/CommerceFlowSystem/CommerceFlowSystemBuilder.cs
@@ -6,5 +6,5 @@ public class CommerceFlowSystemBuilder(IDistributedApplicationBuilder builder) :
     protected override string RepositoriesDomainUrl { get; init; } = GithubDomainUrl;
     protected override string TopDomainLevel { get; init; } = "com";
     protected override string Domain { get; init; } = "commerceflow";
-    protected override string SystemContextDiagramUrl { get; init; } = "https://app.icepanel.io/landscapes/v3MTVXKMPjCfg4LaPENe/";
+    protected override string SystemContextDiagramUrl { get; init; } = "https://s.icepanel.io/8ed1WNhopjo4xF/QQdp";
 }
