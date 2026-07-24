93d9e3500ad6b821aa38d080e63f7e5ddef3f992
Lucas Fogliarini <lucasfogliarini@gmail.com>
2026-07-24 10:29:41 -0300

    add /

--- CommerceFlow.AppHost/CommerceFlowSystem/CommerceFlowSystemBuilder.cs
+++ CommerceFlow.AppHost/CommerceFlowSystem/CommerceFlowSystemBuilder.cs
@@ -6,5 +6,5 @@ public class CommerceFlowSystemBuilder(IDistributedApplicationBuilder builder) :
     protected override string RepositoriesDomainUrl { get; init; } = GithubDomainUrl;
     protected override string TopDomainLevel { get; init; } = "com";
     protected override string Domain { get; init; } = "commerceflow";
-    protected override string SystemContextDiagramUrl { get; init; } = "https://s.icepanel.io/8ed1WNhopjo4xF/QQdp";
+    protected override string SystemContextDiagramUrl { get; init; } = "https://s.icepanel.io/8ed1WNhopjo4xF/QQdp/";
 }
